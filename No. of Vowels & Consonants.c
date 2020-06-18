9587b07e70e7ff3c6e95557056ef24ad571e2338
pranavkalidas <65642040+pranavkalidas@users.noreply.github.com>
2020-06-18 16:23:00 +0530

    Add files via upload

---
+++ No. of Vowels & Consonants.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    char line[150];
+    int vowels, consonant, digit, space;
+    
+    vowels = consonant = digit = space = 0;
+
+    printf("Enter a line of string:\n");
+    fgets(line, sizeof(line), stdin);
+
+    for (int i = 0; line[i] != '\0'; ++i)
+    {
+        if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' ||
+            line[i] == 'o' || line[i] == 'u' || line[i] == 'A' ||
+            line[i] == 'E' || line[i] == 'I' || line[i] == 'O' ||
+            line[i] == 'U')
+            ++vowels;
+        else if ((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z')) 
+            ++consonant;
+    }
+    printf("Vowels: %d", vowels);
+    printf("\nConsonants: %d", consonant);
+    return 0;
+}
